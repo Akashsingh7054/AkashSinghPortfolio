1780027cea68cd7cad4623c0cf47dc9bc1cce721
Akashsingh7054 <950akashsingh@gmail.com>
2025-04-15 20:35:30 +0530

    Last Update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# AkashSinghPortfolio
+# [Akash Singh's Portfolio](https://github.com/Akashsingh7054/)
+
+![Akash Singh's Portfolio-image]()
+
+
+🚀 Welcome to my latest portfolio project! 🚀
+
+This project is a showcase of my skills and projects, developed using HTML5, CSS3, JavaScript, and powered by React.js.
+
+
+## Technologies Used:
+
+- HTML5
+- CSS3
+- JavaScript
+- ReactJS
+
+## Getting Started:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory.
+3. Install dependencies using `npm install`.
+4. Start the development server using `npm run start`.
+5. Open your browser and visit `http://localhost:3000` to view the portfolio.
+
+## Let's Connect:
+
+Feel free to reach out via email at [950akashsingh@gmail.com](mailto:your.email@example.com) or connect with me on [LinkedIn](https://www.linkedin.com/in/akash-singh-76542924b/).
+
---
+++ src/data/constants.js
@@ -0,0 +1,195 @@
+export const Bio = {
+  name: "Akash Singh",
+  roles: ["Full Stack Developer", "Programmer"],
+  description:
+    "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+  github: "https://github.com/Akashsingh7054",
+  resume: "",
+  linkedin: "https://www.linkedin.com/in/akash-singh-76542924b/",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "C",
+        image: "C-logo.png",
+      },
+      {
+        name: "C++",
+        image: "CPP-logo.png",
+      },
+      {
+        name: "Java",
+        image: "java-logo.svg",
+      },
+      {
+        name: "Python",
+        image: "Python-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "html5-logo.png",
+      },
+      {
+        name: "CSS",
+        image: "CSS3-logo.png",
+      },
+
+      {
+        name: "JavaScript",
+        image: "JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "nodejs-logo.svg",
+      },
+      {
+        name: "Mongo DB",
+        image: "mongodb.png",
+      },
+      {
+        name: "Express Js",
+        image: "express.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "git.jpg",
+      },
+      {
+        name: "GitHub",
+        image: "GitHub-logo.png",
+      },
+      {
+        name: "VS Code",
+        image: "vscode-logo.png",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "Ietlogo.png",
+    school: "Institute Of Engineering And Technology Lucknow ",
+    date: "2022 - 2026",
+    grade: "8.3 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering  at IET Institute Of Technology, Lucknow with a CGPA of 8.37. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree:
+      "Bachelor of Technology - BTech, Computer Science and Engineering(AI)",
+  },
+  {
+    id: 1,
+    img: "gyandeep.jpg",
+    school: "St. Mary’s Sr. Sec. School  , Jhansi",
+    // date: "2021 - 2022",
+    grade: " 90.2%",
+    desc: "I completed my Intermediate at St. Mary’s Sr. Sec. School  , Jhansi where I studied Maths, Physics and Chemistry stream.",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "gyandeep.jpg",
+    school: "St. Mary’s Sr. Sec. School  , Jhansi",
+    //date: "2019-2020",
+    grade: "88%",
+    desc: "I completed my class 10 education at St. Mary’s Sr. Sec. School  , Jhansi.",
+    degree: "",
+  },
+];
+
+export const coding = [
+  {
+    id: 10,
+    title: "Codforces",
+    description:
+      "I actively participate in Codeforces contests, constantly improving my problem-solving skills and algorithmic thinking. With a max rating of 1340 (Pupil),  I have tackled a wide range of problems",
+    image: "cf.png",
+    tags: ["Highest Rating 1482", " | Specialist"],
+    category: "web app",
+    github: "https://codeforces.com/profile/23j11j",
+    webapp: "https://codeforces.com/profile/23j11j",
+  },
+
+  {
+    id: 10,
+    title: "Codechef",
+    description:
+      "I am an enthusiastic problem solver and an active participant in CodeChef contests, consistently improving my algorithmic thinking and coding efficiency. Currently rated 3★, I have solved a variety of problems spanning.",
+    image: "cc-logo.svg",
+    tags: ["Highest Rating 1817", " | 4 Star"],
+    category: "",
+    github: "https://www.codechef.com/users/akashsingh82",
+    webapp: "#",
+  },
+
+  {
+    id: 10,
+    title: "LeetCode",
+    description:
+      "I actively solve LeetCode problems to strengthen my Data Structures & Algorithms (DSA) knowledge and prepare for technical interviews. As a Knight, I have tackled problems across various difficulty levels.",
+    image: "leetcode.png",
+    tags: ["Highest Rating 1890", " | Knight"],
+    category: "web app",
+    github: "https://leetcode.com/u/950akashsingh/",
+  },
+  {
+    id: 10,
+    title: "AtCoder",
+    description:
+      "I actively solve AtCoder problems to strengthen my Data Structures & Algorithms (DSA) knowledge and prepare for technical interviews. As a Knight, I have tackled problems across various difficulty levels.",
+    image: "atcoder.png",
+    tags: ["Highest Rating 874", " | Green 6Kyu"],
+    category: "web app",
+    github: "https://atcoder.jp/users/akashsingh_22",
+  },
+
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Code Editor",
+    description:
+      "Developed a custom Code Editor application using HTML5, CSS3, JavaScript, ReactJS, and the Google Custom Search API to provide efficient search results.",
+    image: "Google.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "ReactJs"],
+    category: "web app",
+    github: "https://github.com/Akashsingh7054/Code_Editor",
+    webapp: "https://codelive.onrender.com/login",
+  },
+
+  {
+    id: 9,
+    title: "Portfolio",
+    description:
+      "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+    image: "portfolio-logo.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "React"],
+    category: "web app",
+    github: "",
+    webapp: "#",
+  },
+
+ 
+];
+
+export const experience = [{}];
